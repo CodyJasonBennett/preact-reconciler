d620dfefb7da79b69e8a06904e2dd7fc1939e38b
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2023-03-05 12:59:23 -0600

    chore: remove custom canvas

--- demo/index.jsx
+++ demo/index.jsx
@@ -1,19 +1,7 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom/client'
-import * as THREE from 'three'
-import { createRoot, extend } from '@react-three/fiber'
+import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
-
-function Canvas({ children, ...props }) {
-  const [canvas, setCanvas] = React.useState()
-  const root = React.useMemo(() => canvas && createRoot(canvas), [canvas])
-  React.useEffect(() => () => root?.unmount(), [root])
-
-  extend(THREE)
-  root?.render(children, props)
-
-  return <canvas ref={setCanvas} />
-}
 
 ReactDOM.createRoot(root).render(
   <Canvas>

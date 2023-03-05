478ae6bfd8aadfeb4d37a88b920c5461458ad14d
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2023-03-05 17:13:37 -0600

    chore: cleanup demo

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
 import * as path from 'node:path'
-import { defineConfig, transformWithEsbuild } from 'vite'
+import * as vite from 'vite'
+import preact from '@preact/preset-vite'
 
 const entries = ['./src/index.ts', './src/constants.ts', './src/reflection.ts']
 
-export default defineConfig({
+export default vite.defineConfig({
+  root: process.argv[2] ? undefined : 'demo',
+  resolve: {
+    alias: {
+      react: 'preact/compat',
+      'react-dom': 'preact/compat',
+      'react-reconciler': path.resolve(__dirname, './src'),
+    },
+  },
+  optimizeDeps: {
+    exclude: ['react-reconciler'],
+  },
   build: {
     target: 'es2018',
     lib: {
@@ -20,12 +32,13 @@ export default defineConfig({
     },
   },
   plugins: [
+    preact(),
     {
       name: 'vite-minify',
       renderChunk: {
         order: 'post',
         async handler(code, { fileName }) {
-          return transformWithEsbuild(code, fileName, {
+          return vite.transformWithEsbuild(code, fileName, {
             minify: true,
             mangleProps: /^__/,
             mangleQuoted: true,

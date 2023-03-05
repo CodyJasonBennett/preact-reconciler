7b69088ca2a1e3bd3c7524b86ab79e132fe5c06c
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2023-03-04 18:10:33 -0600

    chore: cleanup internals

--- src/index.js
+++ src/index.js
@@ -6,47 +6,44 @@ let i = 0
 export function Reconciler(HostConfig) {
   const id = `preact-fiber-${i++}`
 
+  // Creates an HTMLNode proxy for reconciliation
   class PreactFiber extends HTMLElement {
     setAttribute(name, value) {
       if (name === '__vnode') this[name] = value
     }
+    removeAttribute() {}
+    addEventListener() {}
+    removeEventListener() {}
     appendChild(child) {
       if (!this.__vnode) {
-        HostConfig.appendChildToContainer(this.__containerInfo, child.__vnode.__instance)
+        HostConfig.appendChildToContainer(this.__containerInfo, child.__vnode.stateNode)
       } else {
-        HostConfig.appendChild(this.__vnode.__instance, child.__vnode.__instance)
+        HostConfig.appendChild(this.__vnode.stateNode, child.__vnode.stateNode)
       }
       return super.appendChild(child)
     }
-
     insertBefore(child, beforeChild) {
       if (!this.__vnode) {
-        HostConfig.insertInContainerBefore(
-          this.__containerInfo,
-          child.__vnode.__instance,
-          beforeChild.__vnode.__instance,
-        )
+        HostConfig.insertInContainerBefore(this.__containerInfo, child.__vnode.stateNode, beforeChild.__vnode.stateNode)
       } else {
-        HostConfig.insertBefore(this.__vnode.__instance, child.__vnode.__instance, beforeChild.__vnode.__instance)
+        HostConfig.insertBefore(this.__vnode.stateNode, child.__vnode.stateNode, beforeChild.__vnode.stateNode)
       }
       return super.insertBefore(child, beforeChild)
     }
-
     removeChild(child) {
       if (!this.__vnode) {
-        HostConfig.removeChildFromContainer(this.__containerInfo, child.__vnode.__instance)
+        HostConfig.removeChildFromContainer(this.__containerInfo, child.__vnode.stateNode)
       } else {
-        HostConfig.removeChild(this.__instance, child.__vnode.__instance)
+        HostConfig.removeChild(this.stateNode, child.__vnode.stateNode)
       }
-
       return super.removeChild(child)
     }
   }
-
   customElements.define(id, PreactFiber)
 
   const _vnode = options.vnode
   options.vnode = (vnode) => {
+    // Render all elements as PreactFiber, we'll release any unmanaged nodes later
     if (typeof vnode.type === 'string') {
       vnode.__type = vnode.type
       vnode.type = id
@@ -57,7 +54,8 @@ export function Reconciler(HostConfig) {
 
   const _diffed = options.diffed
   options.diffed = (vnode) => {
-    if (typeof vnode.type === 'string') {
+    if (vnode.__type && vnode.__e instanceof PreactFiber) {
+      // On first run, find the nearest container if able
       let container = vnode.__container
       if (!container) {
         let root = vnode.__
@@ -66,14 +64,15 @@ export function Reconciler(HostConfig) {
       }
 
       if (!container.__containerInfo) {
-        if (vnode.__e instanceof PreactFiber) vnode.__e = document.createElement(vnode.__type)
-        // vnode.type = vnode.__type
+        // Release unmanaged nodes
+        vnode.__e = document.createElement(vnode.__type)
       } else {
+        // Create and link managed nodes
         let next = vnode
         while (next) {
           const node = next
-          if (node.__type && !node.__instance) {
-            node.__instance = HostConfig.createInstance(node.__type, node.props)
+          if (node.__type && !node.stateNode) {
+            node.stateNode = HostConfig.createInstance(node.__type, node.props)
             let ref = node.ref
             Object.defineProperty(node, 'ref', {
               get() {
@@ -81,7 +80,7 @@ export function Reconciler(HostConfig) {
               },
               set(value) {
                 ref = (self) => {
-                  const publicInstance = self === null ? null : HostConfig.getPublicInstance(node.__instance)
+                  const publicInstance = self === null ? null : HostConfig.getPublicInstance(node.stateNode)
                   if (value && 'current' in value) value.current = publicInstance
                   else value?.(publicInstance)
                 }
@@ -92,18 +91,27 @@ export function Reconciler(HostConfig) {
           next = next.__
         }
 
-        if (vnode.__oldProps) {
-          const payload = HostConfig.prepareUpdate(instance, vnode.__type, vnode.__oldProps, vnode.props)
+        // On subsequent runs, reconcile props
+        if (vnode.memoizedProps) {
+          const payload = HostConfig.prepareUpdate(instance, vnode.__type, vnode.memoizedProps, vnode.props)
           if (payload) {
-            const replacement = HostConfig.commitUpdate(instance, payload, vnode.__type, vnode.__oldProps, vnode.props)
+            const replacement = HostConfig.commitUpdate(
+              instance,
+              payload,
+              vnode.__type,
+              vnode.memoizedProps,
+              vnode.props,
+            )
+
+            // If commitUpdate returns an instance, swap to it
             if (replacement) {
-              vnode.__instance = replacement
+              vnode.stateNode = replacement
               if (vnode.ref && 'current' in vnode.ref) vnode.ref.current = replacement
               else vnode.ref?.(replacement)
             }
           }
         }
-        vnode.__oldProps = { ...vnode.props }
+        vnode.memoizedProps = { ...vnode.props }
       }
     }
 

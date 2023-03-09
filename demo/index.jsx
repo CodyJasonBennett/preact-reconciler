1520a62a9aacf89af4a711c14f6956bb1addeff3
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2023-03-08 21:35:52 -0600

    chore: update demo

--- demo/index.jsx
+++ demo/index.jsx
@@ -1,14 +1,15 @@
 import { render } from 'preact'
 import { useState, useEffect } from 'react'
 import { usePlane, useBox, Physics } from '@react-three/cannon'
+import { OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 
 function Plane(props) {
   const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0], ...props }))
   return (
-    <mesh receiveShadow ref={ref}>
-      <planeGeometry args={[1000, 1000]} />
-      <meshStandardMaterial color="#f0f0f0" />
+    <mesh ref={ref} receiveShadow scale={1000}>
+      <planeGeometry />
+      <meshPhongMaterial color="#f0f0f0" />
     </mesh>
   )
 }
@@ -16,9 +17,9 @@ function Plane(props) {
 function Cube(props) {
   const [ref] = useBox(() => ({ mass: 1, ...props }))
   return (
-    <mesh castShadow ref={ref}>
+    <mesh ref={ref} castShadow>
       <boxGeometry />
-      <meshStandardMaterial color="orange" />
+      <meshPhongMaterial color="orange" />
     </mesh>
   )
 }
@@ -30,6 +31,7 @@ function App() {
     <Canvas shadows camera={{ fov: 50, position: [-5, 5, 5] }}>
       <ambientLight />
       <spotLight castShadow angle={0.25} penumbra={0.5} position={[10, 10, 5]} />
+      <OrbitControls />
       <Physics>
         <Plane />
         <Cube position={[0, 5, 0]} />
